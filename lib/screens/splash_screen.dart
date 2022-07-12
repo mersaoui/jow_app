12c11036b45094fce66323ed921787764423aa82
Lukáš Zoubek <zoubek@datasys.cz>
2022-07-12 08:17:16 +0200

    přidání pozadí do splash_screen.dart

--- lib/screens/splash_screen.dart
+++ lib/screens/splash_screen.dart
@@ -28,6 +28,7 @@ class _SplashScreenState extends State<SplashScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: Colors.blueGrey,
       body: Center(
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
