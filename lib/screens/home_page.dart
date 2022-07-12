39b01ca43761d9cedd6dd91a9954aee556173f0d
Lukáš Zoubek <zoubek@datasys.cz>
2022-07-12 08:21:22 +0200

    pokus o vytvoření bool screen

--- lib/screens/home_page.dart
+++ lib/screens/home_page.dart
@@ -9,27 +9,72 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
-  late WebViewController _webViewController;
+  bool isLoading = false;
+  double progressNumber = 0;
+  late WebViewController webViewController;
+
+  @override
+  void initState() {
+    super.initState();
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: SafeArea(
-        child: WillPopScope(
-          onWillPop: () async {
-            if (await _webViewController.canGoBack()) {
-              _webViewController.goBack();
-              return false;
-            } else {
-              return true;
-            }
-          },
-          child: WebView(
-            initialUrl: 'https://jow.plus/live-meta/jow-charki',
-            javascriptMode: JavascriptMode.unrestricted,
-            onWebViewCreated: (webViewController) =>
-            _webViewController = webViewController,
-          ),
+        child: Column(
+          children: [
+            Container(
+              alignment: Alignment.center,height: 300, decoration:BoxDecoration(color: Colors.blueGrey),
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  CircularProgressIndicator(
+                      color: Colors.amberAccent, value: progressNumber),
+                  Container(
+                    height: 180,
+                    width: 180,
+                    color: Colors.blue,
+                  ),
+                  Container(
+                    child: Text('JOW RADIO',
+                        style: TextStyle(
+                            fontSize: 24,
+                            fontWeight: FontWeight.bold,
+                            backgroundColor: Color.fromARGB(255, 15, 60, 97),
+                            color: Colors.white)),
+                  ),
+
+                ],
+              ),
+            ),
+            Container(
+              decoration: BoxDecoration(color: Colors.blueGrey),
+              child: WebView(
+                onPageFinished: (state) {
+                  print('onPageloaded state: $state');
+                  setState(() {
+                    isLoading = false;
+                  });
+                },
+                onPageStarted: (state) {
+                  print('onPageStarted state: $state');
+                  isLoading = true;
+                },
+                onProgress: (progress) {
+                  setState(
+                        () {
+                      progressNumber = progress.toDouble();
+                    },
+                  );
+                },
+                initialUrl: 'https://jow.plus/live-meta/jow-charki',
+                javascriptMode: JavascriptMode.unrestricted,
+                onWebViewCreated: (_webViewController) =>
+                webViewController = _webViewController,
+              ),
+            ),
+          ],
         ),
       ),
     );
