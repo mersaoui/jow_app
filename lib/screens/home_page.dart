4215f4e668772613e5a6f1429eacd70ba336e34d
Lukáš Zoubek <zoubek@datasys.cz>
2022-07-28 08:18:21 +0200

    edit home_page to adding connectivity package

--- lib/screens/home_page.dart
+++ lib/screens/home_page.dart
@@ -1,5 +1,10 @@
+import 'dart:async';
+import 'dart:developer' as developer;
+
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:webview_flutter/webview_flutter.dart';
+import 'package:connectivity_plus/connectivity_plus.dart';
 
 class HomePage extends StatefulWidget {
   const HomePage({Key? key}) : super(key: key);
@@ -12,71 +17,111 @@ class _HomePageState extends State<HomePage> {
   bool isLoading = false;
   double progressNumber = 0;
   late WebViewController webViewController;
+  ConnectivityResult _connectionStatus = ConnectivityResult.none;
+  final Connectivity _connectivity = Connectivity();
+  late StreamSubscription<ConnectivityResult> _connectivitySubscription;
+  bool isOffline = false;
 
   @override
-  void initState() {
+  initState() {
     super.initState();
+    initConnectivity();
+    _connectivitySubscription = Connectivity()
+        .onConnectivityChanged
+        .listen((ConnectivityResult result) {
+      if (result == ConnectivityResult.none) {
+        //there is no any connection
+        setState(() {
+          isOffline = true;
+        });
+      } else if (result == ConnectivityResult.mobile) {
+        //connection is mobile data network
+        setState(() {
+          isOffline = false;
+        });
+      } else if (result == ConnectivityResult.wifi) {
+        //connection is from wifi
+        setState(() {
+          isOffline = false;
+        });
+      } else if (result == ConnectivityResult.ethernet) {
+        //connection is from wired connection
+        setState(() {
+          isOffline = false;
+        });
+      } else if (result == ConnectivityResult.bluetooth) {
+        //connection is from bluetooth threatening
+        setState(() {
+          isOffline = false;
+        });
+      }
+    });
+  }
+
+  @override
+  dispose() {
+    _connectivitySubscription.cancel();
+    super.dispose();
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: SafeArea(
-        child: Column(
-          children: [
-            Container(
-              alignment: Alignment.center,height: 300, decoration:BoxDecoration(color: Colors.blueGrey),
-              child: Column(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  CircularProgressIndicator(
-                      color: Colors.amberAccent, value: progressNumber),
-                  Container(
-                    height: 180,
-                    width: 180,
-                    color: Colors.blue,
-                  ),
-                  Container(
-                    child: Text('JOW RADIO',
-                        style: TextStyle(
-                            fontSize: 24,
-                            fontWeight: FontWeight.bold,
-                            backgroundColor: Color.fromARGB(255, 15, 60, 97),
-                            color: Colors.white)),
-                  ),
-
-                ],
-              ),
-            ),
-            Container(
-              decoration: BoxDecoration(color: Colors.blueGrey),
-              child: WebView(
-                onPageFinished: (state) {
-                  print('onPageloaded state: $state');
-                  setState(() {
-                    isLoading = false;
-                  });
-                },
-                onPageStarted: (state) {
-                  print('onPageStarted state: $state');
-                  isLoading = true;
-                },
-                onProgress: (progress) {
-                  setState(
-                        () {
-                      progressNumber = progress.toDouble();
-                    },
-                  );
-                },
-                initialUrl: 'https://jow.plus/live-meta/jow-charki',
-                javascriptMode: JavascriptMode.unrestricted,
-                onWebViewCreated: (_webViewController) =>
-                webViewController = _webViewController,
-              ),
-            ),
-          ],
+        child: Container(
+          decoration: BoxDecoration(color: Colors.blueGrey),
+          child: !isOffline
+              ? WebView(
+                  onPageFinished: (state) {
+                    print('onPageloaded state: $state');
+                    setState(() {
+                      isLoading = false;
+                    });
+                  },
+                  onPageStarted: (state) {
+                    print('onPageStarted state: $state');
+                    isLoading = true;
+                  },
+                  onProgress: (progress) {
+                    setState(
+                      () {
+                        progressNumber = progress.toDouble();
+                        print('progress: $progressNumber');
+                      },
+                    );
+                  },
+                  initialUrl: 'https://jow.plus/live-meta/jow-charki',
+                  javascriptMode: JavascriptMode.unrestricted,
+                  onWebViewCreated: (_webViewController) =>
+                      webViewController = _webViewController,
+                )
+              : Container(
+                  child: Text("Offline"),
+                ),
         ),
       ),
     );
+  }
+
+  Future<void> initConnectivity() async {
+    late ConnectivityResult result;
+    try {
+      result = await _connectivity.checkConnectivity();
+    } on PlatformException catch (e) {
+      developer.log('Couldn\'t check connectivity status', error: e);
+      return;
+    }
+
+    if (!mounted) {
+      return Future.value(null);
+    }
+
+    return _updateConnectionStatus(result);
+  }
+
+  Future<void> _updateConnectionStatus(ConnectivityResult result) async {
+    setState(() {
+      _connectionStatus = result;
+    });
   }
 }
