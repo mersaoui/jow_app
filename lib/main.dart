39b2a403093729f4252da383f5e2b9a1de81c1ac
Lukáš Zoubek <zoubek@datasys.cz>
2022-08-08 20:55:39 +0200

    rename splash_screen.dart and add to home in main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,14 +1,14 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
-import 'package:jow_app/spalsh_screen.dart';
+import 'package:jow_app/splash_screen.dart';
 
-
+//import 'package:jow_app/plash.dart';
+//import 'package:jow_app/screens/splash_screen.dart';
 void main() {
   runApp(
-    MyApp(),);
-
+    MyApp(),
+  );
 }
-
 
 class MyApp extends StatelessWidget {
   const MyApp({Key? key}) : super(key: key);
@@ -18,11 +18,10 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     // >>> use any dependency from your dependency manager <<<
     SystemChrome.setSystemUIOverlayStyle(const SystemUiOverlayStyle(
-      //statusBarColor: Color.fromARGB(255, 16, 29, 46),  // android statubar
-      //systemNavigationBarColor: Color.fromARGB(255, 16, 29, 46) ////  android  navigator bar
-      // statusBarIconBrightness: Brightness.light, //android icon batterie ....
-        statusBarBrightness: Brightness.dark
-    ));
+        //statusBarColor: Color.fromARGB(255, 16, 29, 46),  // android statubar
+        //systemNavigationBarColor: Color.fromARGB(255, 16, 29, 46) ////  android  navigator bar
+        // statusBarIconBrightness: Brightness.light, //android icon batterie ....
+        statusBarBrightness: Brightness.dark));
     return MaterialApp(
       title: 'Flutter Demo for imy',
       theme: ThemeData(
